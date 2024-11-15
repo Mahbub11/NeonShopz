439709e855835b6bfe25c0f05f978ef14ef2f3e8
Mahbub11 <mahbubrahim926@gmail.com>
2024-11-16 00:25:54 +0600

    fffg

--- public/index.ts
+++ public/index.ts
@@ -1,11 +1,11 @@
-export { default as sideShowOne } from './Images/sideShow.jpg';
-export { default as categoryOne } from './Images/category_one.png';
-export { default as categoryTwo } from './Images/category_two.png';
-export { default as categoryThree } from './Images/category_three.png';
+export { default as sideShowOne } from './images/sideShow.jpg';
+export { default as categoryOne } from './images/category_one.png';
+export { default as categoryTwo } from './images/category_two.png';
+export { default as categoryThree } from './images/category_three.png';
 
 //feature
-export { default as featureOne } from './Images/featureOne_.webp';
-export { default as featuteTwo } from './Images/featureTwo_.webp';
+export { default as featureOne } from './images/featureOne_.webp';
+export { default as featuteTwo } from './images/featureTwo_.webp';
 export { default as featureThree } from './Images/featureThree_.webp';
 
-export { default as productBananr } from './Images/productBannar.webp';
+export { default as productBananr } from './images/productBannar.webp';
